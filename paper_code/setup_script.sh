07daf5b4152abf03bb94f7768720ad97a698d99a
w4nderlust <w4nderlust@gmail.com>
2020-04-02 13:19:36 -0700

    Fixed setup fle

--- paper_code/setup_script.sh
+++ paper_code/setup_script.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
 python -m pip install requirements.txt
-g#it clone https://github.com/circulosmeos/gdown.pl.git
-c#d gdown.pl
-.#/gdown.pl https://drive.google.com/open?id=15TvAxA8TS8nn1lCzpVPn-Myp5RDlJiHF gpt2
-u#nzip gpt2
-m#v gpt2_pt_models ../. 
-c#d .. 
- #
- #
- #
- #
- #
- #
- #
- #
- #
- #
- #
- #
- #
+git clone https://github.com/circulosmeos/gdown.pl.git
+cd gdown.pl
+./gdown.pl https://drive.google.com/open?id=15TvAxA8TS8nn1lCzpVPn-Myp5RDlJiHF gpt2
+unzip gpt2
+mv gpt2_pt_models ../.
+cd ..
